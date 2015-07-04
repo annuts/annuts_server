dcf2e3f2ccbc8b7f4af53c9ed3a160bd8403dfee
dewei <zhangdewei@breadtrip.com>
2015-07-04 22:45:51 +0800

    添加把django对象序列化的方法

--- annuts_app/common/base_utils.py
+++ annuts_app/common/base_utils.py
@@ -4,6 +4,7 @@ __author__ = 'zhangdewei'
 from django.http import HttpResponse
 from django.views.generic import View
 from django.middleware.csrf import get_token
+from django.core import serializers
 
 import json
 
@@ -19,3 +20,13 @@ class BaseView(View):
             'csrf_token': get_token(request)
         }
         return data
+
+def obj_serializers(model_obj, muti=False, type='json'):  # django的model对象转成json形式,muti代表model_obj是单个多想还是多个
+    if muti == True:
+        data = serializers.serialize(type, model_obj)
+        return json.loads(data)
+    else:
+        print 'false'
+        data = serializers.serialize(type, [model_obj])
+        print 'false2'
+        return json.loads(data)[0]
